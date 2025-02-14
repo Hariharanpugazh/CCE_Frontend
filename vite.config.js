82c9e78a48eda306f20c0375370ed5422ae96161
Harlee <hariharanpugazh@gmail.com>
2025-02-14 15:28:07 +0530

    check

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://cce-backend-54k0.onrender.com', // Replace with your Render backend URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: true,
-      },
-    },
-  },
 })
